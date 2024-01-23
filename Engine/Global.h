9690f82671f318add143032c7fafddfa68abeeef
頼慶 八窪 <220630@jc-21.jp>
2024-01-23 13:58:02 +0900

    方向を表すグローバルヘッダ作成した

--- Engine/Global.h
+++ Engine/Global.h
@@ -5,3 +5,13 @@
 #define SAFE_DELETE(p) {if ((p)!=nullptr) { delete (p); (p)=nullptr;}}
 #define SAFE_DELETE_ARRAY(p) {if ((p)!=nullptr) { delete[] (p); (p)=nullptr;}}
 #define SAFE_RELEASE(p) {if ((p)!=nullptr) { p->Release(); (p)=nullptr;}}
+
+namespace Constants 
+{
+    static constexpr XMFLOAT3 UP_VECTOR = { 0.0f, 1.0f, 0.0f };
+    static constexpr XMFLOAT3 DOWN_VECTOR = { 0.0f, -1.0f, 0.0f };
+    static constexpr XMFLOAT3 RIGHT_VECTOR = { 1.0f, 0.0f, 0.0f };
+    static constexpr XMFLOAT3 LEFT_VECTOR = { -1.0f, 0.0f, 0.0f };
+    static constexpr XMFLOAT3 FORWARD_VECTOR = { 0.0f, 0.0f, 1.0f };
+    static constexpr XMFLOAT3 BACKWARD_VECTOR = { 0.0f, 0.0f, -1.0f };
+}
