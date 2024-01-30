80c2b74500a50c42f90abbe1a4170d3ea7dcf757
yoshinpey <yoshi20030613@gmail.com>
2024-01-31 01:07:37 +0900

    Revert "クラス化してみたけどやっぱ微妙"
    This reverts commit d4f806e7588f4871f3e45dbc0a8e8a66c57360d1.

--- InputManager.h
+++ InputManager.h
@@ -1,80 +1,49 @@
 #pragma once
 #include "Engine/Input.h"
-#include "json.hpp"
-#include <fstream>
-#include <string>
 
-class InputManager
+struct ButtonConfig;
+
+// 入力状況を管理する
+namespace InputManager
 {
-private:
-    // マウス入力
-    int MOUSE_LEFT;        // マウス左ボタン
-    int MOUSE_RIGHT;       // マウス右ボタン
-    int SHOOT_KEY;         // 射撃
-    int WEAPON_ACTION_KEY; // 武器アクション
-
-    // キーボード入力
-    int MOVE_FORWARD_KEY;    // 前進
-    int MOVE_LEFT_KEY;       // 左移動
-    int MOVE_BACKWARD_KEY;   // 後退
-    int MOVE_RIGHT_KEY;      // 右移動
-    int JUMP_KEY;            // ジャンプ
-    int RUN_KEY;             // 走る
-    int RELOAD_KEY;          // リロード
-    int MENU_KEY;            // メニュー
-    int EVENT_ACTION_KEY;    // イベントアクション
-    int ABILITY_KEY;         // アビリティ
-
-    // 設定ファイルのパス
-    const char* CONFIG_FILE_PATH = "input_config.json";
-
-    // ボタン配置の設定を読み込む関数
-    void LoadButtonConfig();
-
-public:
-    using json = nlohmann::json;
-    // 初期化関数
-    void Initialize();
-
-    // 更新関数
-    void Update();
 
     // 射撃
-    bool IsShoot() const;
+    bool IsShoot();
 
     // 武器アクション
-    bool IsWeaponAction() const;
+    bool IsWeaponAction();
 
     // 前進
-    bool IsMoveForward() const;
+    bool IsMoveForward();
 
     // 左移動
-    bool IsMoveLeft() const;
+    bool IsMoveLeft();
 
     // 後退
-    bool IsMoveBackward() const;
+    bool IsMoveBackward();
 
     // 右移動
-    bool IsMoveRight() const;
+    bool IsMoveRight();
+
 
     // 移動中
-    bool IsWalk() const;
+    bool IsWalk();
 
     // 走っている
-    bool IsRun() const;
+    bool IsRun();
 
     // ジャンプ中
-    bool IsJump() const;
+    bool IsJump();
 
     // リロード中
-    bool IsReload() const;
+    bool IsReload();
 
     // メニューを開いている
-    bool IsMenu() const;
+    bool IsMenu();
 
     // イベントアクション
-    bool IsEventAction() const;
+    bool IsEventAction();
 
     // アビリティを使用
-    bool IsAbility() const;
+    bool IsAbility();
 };
