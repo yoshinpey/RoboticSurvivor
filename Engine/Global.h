c7e15ee926c39d8725367ca93db343ad9f08c5a5
yoshinpey <yoshi20030613@gmail.com>
2024-02-12 18:17:08 +0900

    INIファイルからFLOATの値を取れるようにした

--- Engine/Global.h
+++ Engine/Global.h
@@ -23,3 +23,35 @@ static void NormalizeFloat3(XMFLOAT3& vec)
     v = XMVector3Normalize(v);
     XMStoreFloat3(&vec, v);
 }
+
+// INIファイルから浮動小数点数を取得する関数
+static float GetPrivateProfileFloat(LPCTSTR lpAppName, LPCTSTR lpKeyName, LPCSTR lpDefault, LPCTSTR lpFileName)
+{
+    TCHAR buffer[256];
+    GetPrivateProfileString(lpAppName, lpKeyName, lpDefault, buffer, sizeof(buffer), lpFileName);
+
+    // 文字列から浮動小数点数に変換
+    float result = std::atof(buffer);
+
+    // 変換エラーが発生した場合はデフォルト値を返す
+    if (result == 0 && buffer[0] != '0')
+    {
+        result = std::atof(lpDefault);
+    }
+
+    return result;
+}
+/*
+//iniファイルからfloat型の変数を取ってくる
+static float GetPrivateProfilefloat(LPCTSTR lpAppName, LPCTSTR lpKeyName, LPCTSTR lpDefault, LPCTSTR lpFileName)
+{
+    char caption[64];
+    int len = GetPrivateProfileString(lpAppName, lpKeyName, lpDefault, caption, 64, lpFileName);
+
+    //情報が取れているなら
+    if (len)
+        return strtof(caption, NULL);   //取った情報を返す
+    else
+        return strtof(lpDefault, NULL); //Defaultの情報を返す
+}
+*/
