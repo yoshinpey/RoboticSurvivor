3e3508f8ac6da58c3178ed3f380d141f1e72ecbe
頼慶 八窪 <220630@jc-21.jp>
2024-02-13 09:44:50 +0900

    XMFLOAT3型の2点間距離を計算する関数と、XMFLOAT3型の2点から向きベクトルを計算する関数を作成した。

--- Engine/Global.h
+++ Engine/Global.h
@@ -6,15 +6,13 @@
 #define SAFE_DELETE_ARRAY(p) {if ((p)!=nullptr) { delete[] (p); (p)=nullptr;}}
 #define SAFE_RELEASE(p) {if ((p)!=nullptr) { p->Release(); (p)=nullptr;}}
 
-namespace Constants 
-{
-    static constexpr XMFLOAT3 UP_VECTOR = { 0.0f, 1.0f, 0.0f };
-    static constexpr XMFLOAT3 DOWN_VECTOR = { 0.0f, -1.0f, 0.0f };
-    static constexpr XMFLOAT3 RIGHT_VECTOR = { 1.0f, 0.0f, 0.0f };
-    static constexpr XMFLOAT3 LEFT_VECTOR = { -1.0f, 0.0f, 0.0f };
-    static constexpr XMFLOAT3 FORWARD_VECTOR = { 0.0f, 0.0f, 1.0f };
-    static constexpr XMFLOAT3 BACKWARD_VECTOR = { 0.0f, 0.0f, -1.0f };
-}
+// 方向ベクトル
+static constexpr XMFLOAT3 UP_VECTOR = { 0.0f, 1.0f, 0.0f };
+static constexpr XMFLOAT3 DOWN_VECTOR = { 0.0f, -1.0f, 0.0f };
+static constexpr XMFLOAT3 RIGHT_VECTOR = { 1.0f, 0.0f, 0.0f };
+static constexpr XMFLOAT3 LEFT_VECTOR = { -1.0f, 0.0f, 0.0f };
+static constexpr XMFLOAT3 FORWARD_VECTOR = { 0.0f, 0.0f, 1.0f };
+static constexpr XMFLOAT3 BACKWARD_VECTOR = { 0.0f, 0.0f, -1.0f };
 
 // 正規化
 static void NormalizeFloat3(XMFLOAT3& vec)
@@ -22,6 +20,25 @@ static void NormalizeFloat3(XMFLOAT3& vec)
     XMVECTOR v = XMLoadFloat3(&vec);
     v = XMVector3Normalize(v);
     XMStoreFloat3(&vec, v);
+}
+
+// XMFLOAT3型の2点間距離を計算する関数 
+// 戻り値 = |point2 - point1|
+static float CalculateDistance(const XMFLOAT3& point1, const XMFLOAT3& point2)
+{
+    // 1度ベクトルに変換した方がSIMD演算(同時演算)を活用できるため効率的らしい
+    // ベクトルの長さを計算して距離を取得
+    return XMVectorGetX(XMVector3Length(XMVectorSubtract(XMLoadFloat3(&point2), XMLoadFloat3(&point1))));
+}
+
+// XMFLOAT3型の2点から向きベクトルを計算する関数
+// 戻り値 = ポイント1からポイント2へ向かう方向ベクトル
+static XMFLOAT3 CalculateDirection(const XMFLOAT3& point1, const XMFLOAT3& point2)
+{
+    // XMVECTORをXMFLOAT3に変換して返す
+    XMFLOAT3 direction;
+    XMStoreFloat3(&direction, XMVector3Normalize(XMVectorSubtract(XMLoadFloat3(&point2), XMLoadFloat3(&point1))));
+    return direction;
 }
 
 // INIファイルからfloatを取得する関数
