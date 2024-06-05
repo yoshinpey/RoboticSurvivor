fe67bba164120646cf20b269cd0108c0a58d60db
頼慶 八窪 <220630@jc-21.jp>
2024-06-05 10:10:44 +0900

    スポーン変更中

--- EnemySpawn.h
+++ EnemySpawn.h
@@ -2,27 +2,18 @@
 #include "jsonReader.h"
 
 class EnemyManager;
-
-struct WaveData
-{
-    int enemyCount;           // 出現させる敵の数
-    EnemyType enemyType;      // 出現させる敵の種類
-    XMFLOAT3 minPosition;     // 出現させる敵の最小座標
-    XMFLOAT3 maxPosition;     // 出現させる敵の最大座標
-};
+class Player;
 
 class EnemySpawn 
 {
 private:
-    EnemyManager* pEnemyManager_; // 敵を管理するEnemyManagerへのポインタ
-
-    // ウェーブごとに敵を生成する
-    void SpawnEnemiesForWave(const nlohmann::json& waveData);
+    EnemyManager* pEnemyManager_;
+    Player* pPlayer_;
 
 public:
-    EnemySpawn(EnemyManager* enemyManager);
+    EnemySpawn(EnemyManager* enemyManager, Player* player);
     ~EnemySpawn();
 
     // JSONファイルから敵の出現データを読み込み、敵を生成する
-    void SpawnEnemiesFromJSON(const std::string& filename);
+    void SpawnEnemies();
 };
