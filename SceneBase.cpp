b546623dda08d7207352a438c033f424d698f9a8
220630 <220630@jc-21.jp>
2024-07-03 13:25:04 +0900

    save

--- SceneBase.cpp
+++ SceneBase.cpp
@@ -1,1 +1,49 @@
 #include "SceneBase.h"
+#include "UIManager.h"
+#include "AudioManager.h"
+
+SceneBase::SceneBase(GameObject* parent, std::string name)
+	: GameObject(parent, name)
+{
+}
+
+SceneBase::~SceneBase()
+{
+	for (auto u : pUIManagerList_)
+	{
+		delete u;
+	}
+	pUIManagerList_.clear();
+	//AudioManager::Release();
+
+}
+
+void SceneBase::AddUIManager(UIManager* ma)
+{
+	pUIManagerList_.push_back(ma);
+}
+
+void SceneBase::UIUpdate()
+{
+	if (!pUIManagerList_.empty())
+		pUIManagerList_.back()->Update();
+
+	// UI??delete
+	for (auto iter = pUIManagerList_.begin(); iter != pUIManagerList_.end();) {
+		if ((*iter)->GetUIState() == UIManager::UI_STATE::ENDDRAW)
+		{
+			delete* iter;
+			iter = pUIManagerList_.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
+}
+
+void SceneBase::UIDraw()
+{
+	if (!pUIManagerList_.empty())
+		pUIManagerList_.back()->Draw();
+}
--- SceneBase.h
+++ SceneBase.h
@@ -1,5 +1,24 @@
-#pragma once
-class SceneBase
-{
-};
-
+//#pragma once
+//#include "Engine/GameObject.h"
+//#include <vector>
+//
+//class UIManager;
+//
+//class SceneBase : public GameObject
+//{
+//protected:
+//    std::vector<UIManager*> pUIManagerList_;
+//
+//public:
+//    SceneBase(GameObject* parent, std::string name);
+//    virtual ~SceneBase();
+//    virtual void Initialize() override = 0 {};
+//    virtual void Update() override = 0 {};
+//    virtual void Draw() override = 0 {};
+//    virtual void Release() override = 0 {};
+//    void AddUIManager(UIManager* ma);
+//
+//    void UIUpdate();
+//    void UIDraw();
+//
+//};
